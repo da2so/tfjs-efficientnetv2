fc4d1b2a3df70cad16616b603f57c84554cf2186
gangsinhan <kangsinhan@nate.com>
2022-04-02 21:43:10 +0900

    Updates

---
+++ precache-manifest.dbcdd25514e616ab30f38f351b8b7f35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2145a3c25613879dc832",
+    "url": "/tfjs-efficientnetv2-example/static/css/main.fad6505a.chunk.css"
+  },
+  {
+    "revision": "2145a3c25613879dc832",
+    "url": "/tfjs-efficientnetv2-example/static/js/main.2145a3c2.chunk.js"
+  },
+  {
+    "revision": "7b3b39d190b7874e1338",
+    "url": "/tfjs-efficientnetv2-example/static/js/1.7b3b39d1.chunk.js"
+  },
+  {
+    "revision": "488eb15467022aadb269",
+    "url": "/tfjs-efficientnetv2-example/static/js/runtime~main.488eb154.js"
+  },
+  {
+    "revision": "d6cbe1b07a20e58d45059ef5fd566568",
+    "url": "/tfjs-efficientnetv2-example/index.html"
+  }
+];
